673a27bdcaa2dc9f8caefe8a28e45f1231a67acb
Kayla Perez <kayla.n.perez404@gmail.com>
2025-01-02 14:06:32 -0800

    styled navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
   //   const [date, setDate] = useState(new Date());
@@ -29,17 +30,38 @@ export default function NavBar() {
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand`}
           aria-labelledby={`offcanvasNavbarLabel-expand`}
-          placement="start"
+          placement="top"
         >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id={`offcanvasNavbarLabel-expand`}>
-              Offcanvas
+          <Offcanvas.Header closeButton className="pb-1">
+            <Offcanvas.Title
+              className="fs-3"
+              id={`offcanvasNavbarLabel-expand`}
+            >
+              The First Sound
             </Offcanvas.Title>
           </Offcanvas.Header>
-          <Offcanvas.Body>
+          <Offcanvas.Body className="pb-3 overflow-y-hidden">
             <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavLink className={`${styles.navLink} fs-5 my-2`} to="/">
+                <i class="bi bi-house-fill"></i> Home
+              </NavLink>
+              <NavLink className={`${styles.navLink} fs-5 my-2`} to="/about">
+                <i class="bi bi-question-circle-fill"></i> About
+              </NavLink>
+              <NavLink
+                className={`${styles.navLink} fs-5 mt-2 mb-3 `}
+                to="/artists"
+              >
+                <i class="bi bi-brush-fill"></i> Artists
+              </NavLink>
+              <a
+                className={`${styles.navLink} fs-5`}
+                href="https://x.com/firstsoundzine"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i class="bi bi-twitter"></i> Twitter
+              </a>
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>

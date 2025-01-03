072f53d158f519db57d79a80a1d201f9b03ea9ee
Kayla Perez <kayla.n.perez404@gmail.com>
2025-01-03 14:04:42 -0800

    redesigned and added default colors

--- src/components/Window.js
+++ src/components/Window.js
@@ -2,31 +2,33 @@ function WindowTopBar({ title, bgColor, borderColor, children }) {
   const style = {
     backgroundColor: bgColor,
     borderBottom: `5px solid ${borderColor}`,
-    borderTopLeftRadius: '5px',
-    borderTopRightRadius: '5px',
+    borderTopLeftRadius: '2px',
+    borderTopRightRadius: '2px',
   };
 
   const iconStyle = {
-    backgroundColor: 'white',
-    color: 'rgb(186, 195, 206)',
-    border: '2px solid rgb(186, 195, 206)',
-    borderRadius: '5px',
+    // backgroundColor: 'white',
+    // color: 'rgb(186, 195, 206)',
+    // border: '2px solid rgb(186, 195, 206)',
+    // borderRadius: '5px',
+    color: 'white',
   };
 
   return (
     <>
       <div
-        className="p-1  d-flex align-items-center justify-content-between"
+        className=" d-flex align-items-center justify-content-between"
         style={style}
       >
         <h6 className="ms-2 mt-1">{title}</h6>
-        <div className="fs-5 d-flex align-items-center justify-content-end">
+        <div className="fs-6 d-flex align-items-center justify-content-end">
           <i
             style={iconStyle}
             className="bi bi-dash-lg align-self-end px-1"
           ></i>
           <i style={iconStyle} className="bi bi-square px-1 ms-1"></i>
-          <i style={iconStyle} className="bi bi-x-lg window-icon px-1 mx-1"></i>
+          <i style={iconStyle} className="bi bi-x-lg window-icon px-1 ms-1"></i>
+          <p>&nbsp;</p>
         </div>
       </div>
       {children}
@@ -36,18 +38,19 @@ function WindowTopBar({ title, bgColor, borderColor, children }) {
 
 export default function Window({
   title,
-  barBgColor,
-  bgColor,
-  borderColor,
+  barBgColor = '#61ceff',
+  bgColor = 'rgba(9, 73, 170, 0.6)',
+  borderColor = '#00b0ff',
+  fullWidth = false,
   children,
 }) {
   return (
     <div
-      className="shadow"
+      className={`shadow ${fullWidth ? 'd-block' : 'd-inline-block'} mb-3`}
       style={{
         backgroundColor: bgColor,
         border: `5px solid ${borderColor}`,
-        borderRadius: '10px',
+        borderRadius: '7px',
       }}
     >
       <WindowTopBar
@@ -55,7 +58,7 @@ export default function Window({
         bgColor={barBgColor}
         borderColor={borderColor}
       >
-        <div className="p-4">{children}</div>
+        {children}
       </WindowTopBar>
     </div>
   );

2469b7810e0e1cff295d7fbe9cc6d19e8be062c6
Kayla Perez <kayla.n.perez404@gmail.com>
2026-03-07 16:44:34 -0800

    added new component for a fade in effect

---
+++ src/components/FadeInSection.js
@@ -0,0 +1,26 @@
+import { useState, useRef, useEffect } from 'react';
+
+function FadeInSection(props, showOnce = true) {
+  const [isVisible, setVisible] = useState(false);
+  const domRef = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        setVisible(entry.isIntersecting);
+      });
+    });
+    observer.observe(domRef.current);
+    const cleanUp = domRef.current;
+    return () => observer.unobserve(cleanUp);
+  }, [showOnce]);
+  return (
+    <div
+      className={`fadeInSection ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default FadeInSection;

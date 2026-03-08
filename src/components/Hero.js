e00a4114b1cc188733f721185cde7cd01b717ddf
Kayla Perez <kayla.n.perez404@gmail.com>
2026-03-07 16:45:18 -0800

    added sheen effect

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import styles from './Hero.module.css';
 function Hero() {
   return (
     <section
-      className={`${styles.hero} px-4 py-5 text-center d-flex justify-content-center align-items-center`}
+      className={`${styles.hero} ${styles.shineButton} px-4 py-5 text-center d-flex justify-content-center align-items-center`}
     >
       <h1 className="mx-4 p-3 fw-bold">
         Experience the First Sounds of Hatsune Miku
